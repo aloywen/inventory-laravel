23140be80091723a96700b5e528328570dd734ee
aloywen <aliianwar7@gmail.com>
2024-03-08 06:38:21 +0700

    func: create data supplier

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Supplier;
+
 
 class SupplierController extends Controller
 {
@@ -10,7 +12,22 @@ class SupplierController extends Controller
     {
         $data = [
             'title' => 'Data Supplier',
+            'supplier' => Supplier::all(),
         ];
         return view('admin.supplier.index', $data);
+    }
+
+    public function store(Request $request)
+    {
+
+        $data = [
+            'kode_supplier' => $request->kode_supplier,
+            'nama_supplier' => $request->nama_supplier,
+            'alamat' => $request->alamat_supplier,
+        ];
+
+        $data = Supplier::create($data);
+
+        return redirect()->back()->with('supplierStore', 'Supplier berhasil ditambah!');
     }
 }
--- resources/views/admin/supplier/index.blade.php
+++ resources/views/admin/supplier/index.blade.php
@@ -5,32 +5,32 @@
 {{-- TITLE --}}
 <div class="page-title">
 
-    @if (session('bmasukStore'))
+    @if (session('supplierStore'))
         <div class="alert alert-success alert-dismissible show fade">
-            {{ session('bmasukStore') }}
+            {{ session('supplierStore') }}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
 
-    @if (session('bmasukUpdate'))
+    @if (session('supplierUpdate'))
         <div class="alert alert-success alert-dismissible show fade">
-            {{ session('bmasukUpdate') }}
+            {{ session('supplierUpdate') }}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
 
-    @if (session('bmasukDelete'))
+    @if (session('supplierDelete'))
         <div class="alert alert-success alert-dismissible show fade">
-            {{ session('bmasukDelete') }}
+            {{ session('supplierDelete') }}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
 
     <div class="row">
         <div class="col-12 col-md-6 order-md-1 order-last">
-            <a href="{{ route('bmasukAdd'); }}"><button type="button" class="btn btn-primary">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModalLong">
                 Tambah Supplier
-            </button></a>
+            </button>
 
         </div>
     </div>
@@ -48,42 +48,127 @@
             <table class="table table-striped" id="table1">
                 <thead>
                     <tr>
-                        <th>Nomor Transaksi</th>
+                        <th>Kode Supplier</th>
                         <th>Nama Supplier</th>
-                        <th>Tanggal Transaksi</th>
+                        <th>Alamat</th>
                         <th></th>
                     </tr>
                 </thead>
                 <tbody>
-                    {{-- <?php $no = 1 ?>
-                    @foreach ($data as $u)
+                    <?php $no = 1 ?>
+                    @foreach ($supplier as $u)
                     <tr>
-                        <td>{{ $u->no_transaksi }}</td>
                         <td>{{ $u->kode_supplier }}</td>
-                        <td>{{ $u->tgl_transaksi }}</td>
+                        <td>{{ $u->nama_supplier }}</td>
+                        <td>{{ $u->alamat }}</td>
                         <td>
-                            <?php
-                                $no_trans = str_replace('/', '', $u->no_transaksi);
-                                ?>
-                            <a href="{{ route('bmasukEdit',$no_trans) }}">
+                            {{-- <a href="{{ route('bmasukEdit',$no_trans) }}"> --}}
                                 <button class="btn badge bg-warning">
                                     Lihat
                                 </button>
-                            </a>
-                            <a href="{{ route('bmasukDelete',$u->id) }}">
+                            {{-- </a> --}}
+                            <a href="{{ route('supplierDelete',$u->id) }}">
                                 <button type="submit" class="btn badge bg-danger" onclick="return confirm('yakin dihapus?')">
                                     Hapus
                                 </button>
                             </a>
                         </td>
                     </tr>
-                    @endforeach --}}
+                    @endforeach
                 </tbody>
             </table>
         </div>
     </div>
 </section>
 
+   {{-- modal tambah data --}}
+   <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <form action="{{ route('supplierStore') }}" method="POST">
+            @csrf
+            <div class="modal-content">
+                <div class="modal-header bg-primary">
+                    <h5 class="modal-title text-white" id="exampleModalLongTitle">Tambah Data Supplier</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal"
+                        aria-label="Close">
+                        <i data-feather="x"></i>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group" id="kode_b">
+                        <label for="kode_supplier">Kode Supplier</label>
+                        <input type="text" id="kode_supplier" class="form-control" name="kode_supplier" required autocomplete="off" value="{{ old('kode_supplier') }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="nama_supplier">Nama Supplier</label>
+                        <input type="text" id="nama_supplier" class="form-control" name="nama_supplier" required autocomplete="off" value="{{ old('nama_supplier') }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="alamat_supplier">Alamat</label>
+                        <input type="text" id="alamat_supplier" class="form-control" name="alamat_supplier" required autocomplete="off" value="{{ old('alamat_supplier') }}">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-light-secondary"
+                        data-bs-dismiss="modal">
+                        <i class="bx bx-x d-block d-sm-none"></i>
+                        <span class="d-none d-sm-block">Tutup</span>
+                    </button>
 
+                    <button type="submit" class="btn btn-success ms-1" data-bs-dismiss="modal">
+                        <i class="bx bx-check d-block d-sm-none"></i>
+                        <span class="d-none d-sm-block">Simpan</span>
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+{{-- modal edit data --}}
+@foreach ($supplier as $u)
+<div class="modal fade" id="exampleModalLongEdit{{ $u->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <form action="{{ route('supplierUpdate',$u->id) }}" method="POST">
+            @csrf
+            <div class="modal-content">
+                <div class="modal-header bg-primary">
+                    <h5 class="modal-title text-white" id="exampleModalLongTitle">Edit Data Supplier</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal"
+                        aria-label="Close">
+                        <i data-feather="x"></i>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="kode_supplier">Kode Supplier</label>
+                        <input type="text" id="kode_supplier" value="{{ $u->kode_supplier }}" class="form-control" name="kode_supplier" required autocomplete="off">
+                    </div>
+                    <div class="form-group">
+                        <label for="nama_supplier">Nama Supplier</label>
+                        <input type="text" id="nama_supplier" value="{{ $u->nama_supplier }}" class="form-control" name="nama_supplier" required autocomplete="off">
+                    </div>
+                    <div class="form-group">
+                        <label for="alamat_supplier">Alamat</label>
+                        <input type="text" id="alamat_supplier" value="{{ $u->alamat_supplier }}" class="form-control" name="alamat_supplier" required autocomplete="off">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-light-secondary"
+                        data-bs-dismiss="modal">
+                        <i class="bx bx-x d-block d-sm-none"></i>
+                        <span class="d-none d-sm-block">Close</span>
+                    </button>
+
+                    <button type="submit" class="btn btn-primary ms-1" data-bs-dismiss="modal">
+                        <i class="bx bx-check d-block d-sm-none"></i>
+                        <span class="d-none d-sm-block">Accept</span>
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+@endforeach
 
 @endsection
